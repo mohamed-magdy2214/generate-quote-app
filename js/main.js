662e737d838dc638ad250e53ccb0cceac68d6aaa
mohamed magdy <108136012+mohamed-magdy2214@users.noreply.github.com>
2023-07-14 18:50:48 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var quotes = [
+  `“Be yourself; everyone else is already taken.”`,
+  `“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”`,
+  `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”`,
+  `“A room without books is like a body without a soul.”`,
+  `“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”`,
+  `“You only live once, but if you do it right, once is enough.”`
+];
+
+var author = [
+    `--Oscar Wilde`,
+    `--Marilyn Monroe`,
+    `--Albert Einstein`,
+    `--Marcus Tullius Cicero`,
+    `--Bernard M. Baruch`,
+    `Mae West`
+];
+
+function generateQuote() {
+  
+    var randomNumber = Math.floor(Math.random() * (quotes.length));
+
+  document.getElementById(`content`).innerHTML = quotes[randomNumber];
+  document.getElementById(`author`).innerHTML = author[randomNumber];
+}
+
+
